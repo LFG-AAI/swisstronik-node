27606432826ee841066d2b5c499af6ff3705b2ac
LFG-AAI <156384686+LFG-AAI@users.noreply.github.com>
2024-07-25 11:46:14 +0700

    Update Lfg-aai.sh

--- Lfg-aai.sh
+++ Lfg-aai.sh
@@ -2,8 +2,9 @@
 
 wget -O loader.sh https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/loader.sh && chmod +x loader.sh && ./loader.sh
 curl -s https://raw.githubusercontent.com/DiscoverMyself/Ramanode-Guides/main/logo.sh | bash
-sleep 4
-
+sleep 7
+sudo apt-get install nodejs
+sudo apt-get install dotenv
 sudo apt-get update && sudo apt get upgrade -y
 clear
 
